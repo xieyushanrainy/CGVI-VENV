65b124a71782dfecae4e920c258493f07f5defa3
xieyushanrainy <xieyushanrainy@yahoo.com>
2026-03-11 14:34:19 +0000

    incoorperate 90 rotation

--- Assets/Script/Movements/ScaledHandMovement.cs
+++ Assets/Script/Movements/ScaledHandMovement.cs
@@ -5,6 +5,10 @@ public class ScaledHandMovement : MonoBehaviour
     public Transform trackedController;
     public float movementScale = 8f;
 
+    [Tooltip("Euler angle offset applied on top of the controller rotation to preserve " +
+             "the mesh's baked-in initial rotation (e.g. 90° around X).")]
+    public Vector3 modelRotationOffset = new Vector3(90f, 0f, 0f);
+
     Vector3 startControllerPos;
     Vector3 startAnchorPos;
 
@@ -20,6 +24,6 @@ public class ScaledHandMovement : MonoBehaviour
         delta.x *= movementScale;
         delta.z *= movementScale;
         transform.position = startAnchorPos + delta;
-        transform.rotation = trackedController.rotation;
+        transform.rotation = trackedController.rotation * Quaternion.Euler(modelRotationOffset);
     }
 }
