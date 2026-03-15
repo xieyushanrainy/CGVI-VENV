978b08d7244d5e016638f0a61cb7154a84c80221
nkkho <nkkhouni@gmail.com>
2026-03-15 14:48:34 +0000

    Add firework

---
+++ Assets/Script/UI/FireworkSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class FireworkSpawner : MonoBehaviour
+{
+    public GameObject fireworkPrefab; // Your FireworkUI prefab
+    public int fireworksCount = 5;    // Number of fireworks
+    public RectTransform canvasRect;  // Reference to your Canvas RectTransform
+    public float delayBetween = 0.3f; // Delay in seconds between fireworks
+
+    public void PlayMultipleFireworksWithDelay()
+    {
+        StartCoroutine(SpawnFireworksCoroutine());
+    }
+
+    private IEnumerator SpawnFireworksCoroutine()
+    {
+        for (int i = 0; i < fireworksCount; i++)
+        {
+            // Random position inside the canvas
+            float x = Random.Range(-canvasRect.rect.width / 2f, canvasRect.rect.width / 2f);
+            float y = Random.Range(-canvasRect.rect.height / 2f, canvasRect.rect.height / 2f);
+            Vector3 spawnPos = new Vector3(x, y, 0);
+
+            // Instantiate Firework prefab
+            GameObject fw = Instantiate(fireworkPrefab, canvasRect);
+            fw.GetComponent<RectTransform>().anchoredPosition = spawnPos;
+
+            // Play the firework
+            fw.GetComponent<FireworkUIController>().PlayFirework();
+
+            // Wait for delay before spawning the next one
+            yield return new WaitForSeconds(delayBetween);
+        }
+    }
+}
--- Assets/Script/UI/canvasControl.cs
+++ Assets/Script/UI/canvasControl.cs
@@ -6,19 +6,37 @@ public class canvasControl : MonoBehaviour
     public GameObject playUI;
     public GameObject endGameUI;
     public GameObject moleReady;
+    public GameObject hammerReady;
     [SerializeField] TextMeshProUGUI readyText_mole;
+    [SerializeField] TextMeshProUGUI readyText_hammer;
+    [SerializeField] TextMeshProUGUI winner;
 
     void Start()
     {
         playUI.SetActive(false);
         endGameUI.SetActive(false);
-        moleReady.SetActive(true);
+        if (GameData.LocalRole == RoleManager.Role.Mole)
+        {
+            moleReady.SetActive(true);
+            hammerReady.SetActive(false);
+        } else
+        {
+            moleReady.SetActive(false);
+            hammerReady.SetActive(true);
+        }
+        
+        
         
     }
 
     public void moleClicked()
     {
         SetTextTransparency(readyText_mole, 1f);
+    }
+
+    public void hammerClicked()
+    {
+        SetTextTransparency(readyText_hammer, 1f);
     }
 
     private void SetTextTransparency(TextMeshProUGUI text, float alpha)
@@ -33,13 +51,25 @@ public class canvasControl : MonoBehaviour
         playUI.SetActive(true);
         endGameUI.SetActive(false);
         moleReady.SetActive(false);
+        hammerReady.SetActive(false);
         FindObjectOfType<Timer>().startGame();
     }
 
     public void End()
     {
+        string text = FindObjectOfType<ScoreManager>().GetWinner();
+        if (text == "Draw")
+        {
+            winner.text = text;
+        } else
+        {
+            winner.text = $"The winner is {text}!";
+        }
+        
         playUI.SetActive(false);
         endGameUI.SetActive(true);
         moleReady.SetActive(false);
+        hammerReady.SetActive(false);
+        FindObjectOfType<FireworkSpawner>().PlayMultipleFireworksWithDelay();
     }
 }
